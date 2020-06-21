fb980dc81e8a0c4d633dd5309e373f35a6a08135
Ramesh Meduri <rameshmeduri@outlook.com>
2020-06-22 01:22:09 +0800

    init

---
+++ useEffect.js
@@ -0,0 +1,20 @@
+// On Mount and every render
+useEffect(() => {
+  dosomething();
+});
+
+// Only on Mount
+useEffect(() => {
+  dosomething();
+}, []);
+
+// On Mount/every time [count] change
+useEffect(() => {
+  dosomething();
+}, [count]);
+
+// UseEffect with cleanup
+useEffect(() => {
+  dosomething();
+  return clearSomething();
+});
